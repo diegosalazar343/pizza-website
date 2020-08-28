43c2512151db161124a8f43c582a3a9ad23b5115
diegosalazar343 <salazardiego343@gmail.com>
2020-08-28 13:29:17 -0700

    researching

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,37 @@
 //Business Logic
-function determinePrice()
+function determinePrice(input1, input2, input3, input4, input5, input6) {
+  const valTotal = input1 + input2 + input3 + input4 + input5 + input6;
+if (valTotal valTotal <= 7) {
+  price ="7 dollars";
+} else if (valTotal <= 13) {
+  price="13 dollars";
+} else {
+  price="20 dollars";
+}
+  return price;
+}
+
+
 
 //User Logic
 $(document).ready(function() {
-  let pizzaOrder = [];
-  const size = $("#size").val();
-  const crust = $("#crust").val();
-  const sauce = $("#sauce").val();
-  $("input:checkbox[name=cheese-type]:checked").each(function() {
-    const cheeseType = $(this).val();
-    pizzaOrder.push(cheeseType);
-  })
-  $("input:checkbox[name=meat-type]:checked").each(function() {
-    const meatType = $(this).val();
-    pizzaOrder.push(meatType);
-  })
-  $("input:checkbox[name=non-meat-type]:checked").each(function() {
-    const nonMeatType = $(this).val();
-    pizzaOrder.push(noneMeatType);
-  })
-})
+  $("form").submit(function(event){
+    event.preventDefault();
+    let pizzaOrder = [];
+    const size = parseInt($("#size").val());
+    const crust = parseInt($("#crust").val());
+    const sauce = parseInt($("#sauce").val());
+    $("input:checkbox[name=cheese-type]:checked").each(function () {
+      const cheeseType = $(this).val();
+      pizzaOrder.push(cheeseType);
+    })
+    $("input:checkbox[name=meat-type]:checked").each(function () {
+      const meatType = $(this).val();
+      pizzaOrder.push(meatType);
+    })
+    $("input:checkbox[name=non-meat-type]:checked").each(function () {
+      const nonMeatType = $(this).val();
+      pizzaOrder.push(noneMeatType);
+    }
+  });
+});
