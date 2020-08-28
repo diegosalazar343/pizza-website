28b9a7f3431eab23bc8ea46d319c30e40e6ec43e
diegosalazar343 <salazardiego343@gmail.com>
2020-08-28 11:31:07 -0700

    create button side of user logic in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,21 @@
 
 //User Logic
 $(document).ready(function() {
+  let pizzaOrder = [];
+  event.preventDefault();
   const size = $("#size").val();
   const crust = $("#crust").val();
   const sauce = $("#sauce").val();
+  $("input:checkbox[name=cheese-type]:checked").each(function() {
+    const cheeseType = $(this).val();
+    pizzaOrder.push(cheeseType);
+  })
+  $("input:checkbox[name=meat-type]:checked").each(function() {
+    const meatType = $(this).val();
+    pizzaOrder.push(meatType);
+  })
+  $("input:checkbox[name=non-meat-type]:checked").each(function() {
+    const nonMeatType = $(this).val();
+    pizzaOrder.push(noneMeatType);
+  })
 }
